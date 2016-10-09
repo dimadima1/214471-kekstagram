4725c8398c0f5555f65d81fb13f21cada3ad990a
dimadima <mendozzaa@yandex.ru>
2016-10-09 12:18:25 +0300

    ipdate

---
+++ src/js/check.js
@@ -0,0 +1,34 @@
+function getMessage (a, b) {
+	if (typeof a === 'boolean') {
+		if (a === true) {
+			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+		}
+		else {
+			return "Переданное GIF-изображение не анимировано";
+		}
+	}
+	if (typeof a === 'number') {
+		var multiplication = b * 4;
+		return "Переданное SVG-изображение содержит " + a + " объектов и " + multiplication + " атрибутов";
+	}
+	
+	if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0;
+    for(var i = 0; i < a.length; i++) {
+      artifactsSquare += a[i] * b[i];
+} 
+		return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+	}
+	if (Array.isArray (a) ) {
+		var amountOfRedPoints = 0;
+    for(var i = 0; i < a.length; i++) {
+    amountOfRedPoints += a[i];
+    }
+		return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+	}
+else {
+	return 'Переданы некорректные данные';
+    } 
+
+}
+
